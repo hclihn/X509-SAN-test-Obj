8f470e1fc2f750987f24170bdac4ab4e9af4f650
hclihn <23141651+hclihn@users.noreply.github.com>
2024-06-29 00:59:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+import x509 from './x509.js';
+
+const cert = `-----BEGIN CERTIFICATE-----
+    MIIIlTCCCDygAwIBAgIQZNV05YqnQ6CgPBtNhzfNvzAKBggqhkjOPQQDAjCBhDEr
+    MCkGCgmSJomT8ixkAQEMG2lkZW50aXR5OmlkbXMuZ3JvdXAuNTUwNDQ4NzEtMCsG
+    A1UEAwwkYXBwYXV0aG9yaXR5MDAzLmNuZ3lhMDUucGllLnNpbHUubmV0MSYwJAYD
+    VQQLDB1tYW5hZ2VtZW50OmlkbXMuZ3JvdXAuMTQwNTIwNjAeFw0yNDA2MjYyMDM3
+    NTVaFw0yNDA3MDYyMDQyNTVaMIH0MQswCQYDVQQGEwJVUzEnMCUGA1UECwwebWFu
+    YWdlbWVudDppZG1zLmdyb3VwLjEwODYyNzA0MRMwEQYDVQQLDApjbi1lYXN0LTFl
+    MS0wKwYDVQQDDCRlOWQyNTE4My03ZmJhLTQxMDQtOTE0ZS1iZWFmYWU2ZTcyMGIx
+    LDAqBgoJkiaJk/IsZAEBDBxpZGVudGl0eTppZG1zLmdyb3VwLjExMTMzMTg0MSEw
+    HwYDVQQLDBhvd25lcjppZG1zLmdyb3VwLjU1NDgzMTYxJzAlBgNVBAoMHm1hbmFn
+    ZW1lbnQ6aWRtcy5ncm91cC4xMDg2MjcwNDBZMBMGByqGSM49AgEGCCqGSM49AwEH
+    A0IABF9SXaxFQWFUceajrIpwTR58E5WEXE2EGI2oYM4O/OcIXY/ohW8Et3rZmyBt
+    SxTbpVxzw0mIaUkrnu4tECNLBIqjggYcMIIGGDCCA3UGA1UdEQSCA2wwggNogjZj
+    YWJvb2RsZS1nYXRla2VlcGVyLWdjYmQuY2Fib29kbGUtZ2F0ZWtlZXBlci1nY2Jk
+    Lmt1YmWCRyouY2Fib29kbGUtZ2F0ZWtlZXBlci1nY2JkLmNhYm9vZGxlLWdhdGVr
+    ZWVwZXItZ2NiZC5rdWJlLmNsb3VkLnNpbHUubmV0hntmbHVmZnk6Ly9jYWJvb2Rs
+    ZS1nYXRla2VlcGVyLWdjYmQuY2Fib29kbGUtZ2F0ZWtlZXBlci1nY2JkLmt1YmUu
+    MTQwNTIwNi9vd25lcj01NTQ4MzE2L21hbmFnZW1lbnQ9MTA4NjI3MDQvaWRlbnRp
+    dHk9MTExMzMxODSHBKwVE1SHECQC8UAQEAG7AAAAAAAAAA2HBKwVE1SHBKwQsuuH
+    ECQC8UAQKjsNAAAAAAAAABWHBKwWtgiCVGNhYm9vZGxlLWdhdGVrZWVwZXItZ2Ni
+    ZC5jYWJvb2RsZS1nYXRla2VlcGVyLWdjYmQua3ViZS5jbi1lYXN0LTFlLms4cy5j
+    bG91ZC5zaWx1Lm5ldII9Y2Fib29kbGUtZ2F0ZWtlZXBlci1zZXJ2aWNlLWdjYmQu
+    Y2Fib29kbGUtZ2F0ZWtlZXBlci1nY2JkLnN2Y4JgY2Fib29kbGUtZ2F0ZWtlZXBl
+    ci1zZXJ2aWNlLWdjYmQuY2Fib29kbGUtZ2F0ZWtlZXBlci1nY2JkLnN2Yy5rdWJl
+    LmNuLWVhc3QtMWUuazhzLmNsb3VkLnNpbHUubmV0gmIqLmNhYm9vZGxlLWdhdGVr
+    ZWVwZXItc2VydmljZS1nY2JkLmNhYm9vZGxlLWdhdGVrZWVwZXItZ2NiZC5zdmMu
+    a3ViZS5jbi1lYXN0LTFlLms4cy5jbG91ZC5zaWx1Lm5ldIJ5Y2Fib29kbGUtZ2F0
+    ZWtlZXBlci1nY2JkLmNhYm9vZGxlLWdhdGVrZWVwZXItc2VydmljZS1nY2JkLmNh
+    Ym9vZGxlLWdhdGVrZWVwZXItZ2NiZC5zdmMua3ViZS5jbi1lYXN0LTFlLms4cy5j
+    bG91ZC5zaWx1Lm5ldIJWY2Fib29kbGUtZ2F0ZWtlZXBlci1nY2JkLmNhYm9vZGxl
+    LWdhdGVrZWVwZXItc2VydmljZS1nY2JkLmNhYm9vZGxlLWdhdGVrZWVwZXItZ2Ni
+    ZC5zdmMwggGPBgkrBgEEAT+FZwEEggGABIIBfAo2Y2Fib29kbGUtZ2F0ZWtlZXBl
+    ci1nY2JkLmNhYm9vZGxlLWdhdGVrZWVwZXItZ2NiZC5rdWJlEgpjbi1lYXN0LTFl
+    GrMBCAAQABgAIAAoADAAOABAAEokCgpjbi1lYXN0LTFlEhRwbGIucGllLXBsYi5w
+    aWUtcHJvZBgASiQKCmNuLWVhc3QtMWUSFGtub2RlMTEwMy5jbmd5YTA1LmtrGAFi
+    GBIULmNhYm9vZGxlLXByb3h5Lmt1YmUYAGoWUElFIFZJUCAmIEJHUCBOZXR3b3Jr
+    c3gAgAEAigEdCgNTRFISFlBJRSBWSVAgJiBCR1AgTmV0d29ya3MiCAgAEAAYADAA
+    MjZjYWJvb2RsZS1nYXRla2VlcGVyLWdjYmQuY2Fib29kbGUtZ2F0ZWtlZXBlci1n
+    Y2JkLmt1YmUyPmNhYm9vZGxlLWdhdGVrZWVwZXItc2VydmljZS1nY2JkLmNhYm9v
+    ZGxlLWdhdGVrZWVwZXItZ2NiZC5rdWJlMC8GCSsGAQQBP4VnAwQiBCBlbnRpdGxl
+    bWVudHMucHJvZHVjdGlvbi5wbGF0Zm9ybTAmBgkrBgEEAT+FZwIEGQQXc2NoZWR1
+    bGVzaWduZXIua3ViZS5waWUwGQYJKwYBBAE/hWcIBAwECmNuLWVhc3QtMWUwGQYJ
+    KwYBBAE/hWcJBAwMCmNuLWVhc3QtMWUwDAYDVR0TAQH/BAIwADAOBgNVHQ8BAf8E
+    BAMCA6gwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMB0GA1UdDgQWBBT9
+    QItwibra8NPsSPOEMsH2q3F8bDAfBgNVHSMEGDAWgBT1K6k7nOehy/7bi1LBgkSa
+    0mKO4TAKBggqhkjOPQQDAgNHADBEAiAa3Kw/nrtHUT25PxyN/vs8LN3yiLH58LYK
+    B9GDH31aswIgWEMyygX+CfbsGOx/7zowCvbSjbkZ/6srH9Xp7qyXYZw=
+    -----END CERTIFICATE-----
+`;
+
+
+function san(pem_cert /*r*/) {
+    //var pem_cert = r.variables.ssl_client_raw_cert;
+    if (!pem_cert) {
+        return '{"error": "no client certificate"}';
+    }
+
+    var cert = x509.parse_pem_cert(pem_cert);
+    console.log(JSON.stringify(cert, null, 2));
+    // subjectAltName oid 2.5.29.17
+    // return JSON.stringify(x509.get_oid_value(cert, "2.5.29.17")[0]);
+    return x509.get_oid_value(cert, "2.5.29.17")[0];
+}
+
+try {
+  var s = 'SANs:\n';
+  var sans = san(cert);
+  for (var i = 0; i<sans.length; i++) {
+      s += sans[i] + '\n';
+  } 
+  console.log(s);
+} catch(err) {
+  console.log(err);
+}
---
+++ x509.js
@@ -0,0 +1,446 @@
+function asn1_parse_oid(buf) {
+  var oid = [];
+  var sid = 0;
+  var cur_octet = buf[0];
+
+  if (cur_octet < 40) {
+      oid.push(0);
+      oid.push(cur_octet);
+
+  } else if (cur_octet < 80) {
+      oid.push(1);
+      oid.push(cur_octet - 40);
+
+  } else {
+      oid.push(2);
+      oid.push(cur_octet - 80);
+  }
+
+  for (var n = 1; n < buf.length; n++) {
+      cur_octet = buf[n];
+
+      if (cur_octet < 0x80) {
+          sid += cur_octet;
+
+          if (sid > Number.MAX_SAFE_INTEGER)
+              throw "Too big SID value: " + sid;
+
+          oid.push(sid);
+          sid = 0;
+
+      } else {
+          sid += cur_octet & 0x7f; sid <<= 7;
+
+          if (sid > Number.MAX_SAFE_INTEGER)
+              throw "Too big SID value: " + sid;
+      }
+  }
+
+  if (buf.slice(-1)[0] >= 0x80)
+      throw "Last octet in oid buffer has highest bit set to 1";
+
+  return oid.join('.')
+}
+
+function asn1_parse_integer(buf) {
+
+  if (buf.length > 6) {
+      // may exceed MAX_SAFE_INTEGER, lets return hex
+      return asn1_parse_any(buf);
+  }
+
+  var value = 0;
+  var is_negative = false;
+
+  if (buf[0] & 0x80) {
+      is_negative = true;
+      value = buf[0] & 0x7f;
+      var compl_int = 1 << (8 * buf.length - 1)
+
+  } else {
+      value = buf[0];
+  }
+
+  if (buf.length > 1) {
+      for (var n = 1; n < buf.length; n++) {
+          value <<= 8;
+          value += buf[n];
+      }
+  }
+
+  if (is_negative)
+      return value - compl_int;
+  else
+      return value;
+}
+
+function asn1_parse_ascii_string(buf) {
+  return buf.toString();
+}
+
+function asn1_parse_ia5_string(buf) {
+  if (is_ia5(buf))
+      return buf.toString();
+  else
+      throw "Not a IA5String: " + buf;
+}
+
+function asn1_parse_utf8_string(buf) {
+  return buf.toString('utf8');
+}
+
+function asn1_parse_bmp_string(buf) {
+  return asn1_parse_any(buf)
+}
+
+function asn1_parse_universal_string(buf) {
+  return asn1_parse_any(buf)
+}
+
+function asn1_parse_bit_string(buf) {
+  if (buf[0] == 0)
+      return buf.slice(1);
+
+  const shift = buf[0]
+  if (shift > 7)
+      throw "Incorrect shift in bitstring: " + shift;
+
+  let value = Buffer.allocUnsafe(buf.length - 1);
+  let upper_bits = 0;
+  const mask = ((1 << shift) - 1) & 0xff;
+    
+  // shift string right and convert to hex
+  for (let n = 1; n < buf.length; n++) {
+       const val = (buf[n] >> shift) | upper_bits;
+       upper_bits = (buf[n] & mask) << (8 - shift);
+       value.writeUint8(val, n - 1);
+  }
+
+  return value;
+}
+
+function asn1_parse_octet_string(buf) {
+  return asn1_parse_any(buf)
+}
+
+function asn1_parse_any(buf) {
+  return buf.toString('hex')
+}
+
+function is_ia5(buf) {
+  for (var n = 0; n < buf.length; n++) {
+      var s = buf[n];
+      if (s > 0x7e)
+          return false;
+  }
+
+  return true;
+}
+
+function asn1_read_length(buf, pointer) {
+  var s = buf[pointer];
+  if (s == 0x80 || s == 0xff)
+      throw "indefinite length is not supported"
+
+  if (s < 0x80) {
+      // length is less than 128
+      pointer++;
+      return [s, pointer];
+
+  } else {
+      var l = s & 0x7f;
+      if (l > 7)
+          throw "Too big length, exceeds MAX_SAFE_INTEGER: " + l;
+
+      if ((pointer + l) >= buf.length)
+          throw "Went out of buffer: " + (pointer + l) + " " + buf.length;
+
+      var length = 0;
+      for (var n = 0; n < l; n++) {
+          length += Math.pow(256, l - n - 1) * buf[++pointer];
+          if (n == 6 && buf[pointer] > 0x1f)
+              throw "Too big length, exceeds MAX_SAFE_INTEGER";
+      }
+
+      return [length, pointer + 1];
+  }
+}
+
+function format_ipv4(buf) {
+    if (buf.length != 4) {
+        throw "Not an IPv4 address: " + buf;
+    }
+    // IPv4 address
+    const intStrs = [];
+    for (let i = 0; i < buf.length; i++) {
+      intStrs.push(String(buf[i]));
+    }
+    return intStrs.join('.');   
+}
+
+function format_ipv6(buf) {
+    if (buf.length != 16) {
+        throw "Not an IPv6 address: " + buf;
+    }
+    // IPv6 address
+    const data = buf.toString('hex');
+    const fields = [];
+    // group in 4 hex digits with leading 0 removed
+    for (let i = 0; i < data.length; i+=4) {
+      for (let j = 0; j < 4; j++) {
+        if (data.charAt(i+j) != '0') {
+          fields.push(data.substr(i+j, 4-j));
+          break;
+        } else if (j == 3) {
+          fields.push('0');
+        }
+      }
+    }
+    // find the longest consecutive range of 0s
+    let max_range = null, start = -1, end = -1;
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == '0') {
+            if (start == -1) {
+                start = i;
+            }
+        } else if (start >= 0) {
+            end = i;
+            if (max_range === null || end - start > max_range[0]) {
+                max_range = [end - start, start, end];
+            }
+            start = end = -1;
+        }
+    }
+    // use :: for the longest consecutive range of 0s
+    if (max_range !== null && max_range[0] > 1) {
+        return fields.slice(0, max_range[1]).join(':') + '::' + 
+            fields.slice(max_range[2]).join(':');
+    }
+    return fields.join(':');
+}
+
+function asn1_parse_primitive(cls, tag, buf) {
+  if (cls == 0) {
+      switch(tag) {
+      // INTEGER
+      case 0x02: return asn1_parse_integer(buf);
+      // BIT STRING
+      case 0x03:
+         const data = asn1_parse_bit_string(buf);
+         try {
+             return asn1_read(data);
+         } catch(e) {
+             return data.toString('hex');
+         }
+      // OCTET STRING
+      case 0x04:
+         try {
+             return asn1_read(buf);
+         } catch(e) {
+             return asn1_parse_octet_string(buf);
+         }
+      // OBJECT IDENTIFIER
+      case 0x06: return asn1_parse_oid(buf);
+      // UTF8String
+      case 0x0c: return asn1_parse_utf8_string(buf);
+      // TIME
+      case 0x0e:
+      // NumericString
+      case 0x12:
+      // PrintableString
+      case 0x13:
+      // T61String
+      case 0x14:
+      // VideotexString
+      case 0x15:
+         return asn1_parse_ascii_string(buf);
+      // IA5String
+      case 0x16: return asn1_parse_ia5_string(buf);
+      // UTCTime
+      case 0x17:
+      // GeneralizedTime
+      case 0x18:
+      // GraphicString
+      case 0x19:
+      // VisibleString
+      case 0x1a:
+      // GeneralString
+      case 0x1b:
+         return asn1_parse_ascii_string(buf);
+      // UniversalString
+      case 0x1c: return asn1_parse_universal_string(buf);
+      // CHARACTER STRING
+      case 0x1d: return asn1_parse_ascii_string(buf);
+      // BMPString
+      case 0x1e: return asn1_parse_bmp_string(buf);
+      // DATE
+      case 0x1f:
+      // TIME-OF-DAY
+      case 0x20:
+      // DATE-TIME
+      case 0x21:
+      // DURATION
+      case 0x22:
+         return asn1_parse_ascii_string(buf);
+      default: return asn1_parse_any(buf);
+      }
+
+  } else if (cls == 2) { // context specific, non-constructed
+      /* SAN GeneralName ::= CHOICE {
+        otherName                       [0]     OtherName,
+        rfc822Name                      [1]     IA5String,
+        dNSName                         [2]     IA5String,
+        x400Address                     [3]     ORAddress,
+        directoryName                   [4]     Name,
+        ediPartyName                    [5]     EDIPartyName,
+        uniformResourceIdentifier       [6]     IA5String,
+        iPAddress                       [7]     OCTET STRING,
+        registeredID                    [8]     OBJECT IDENTIFIER 
+      }*/
+      switch(tag) {
+      case 0x00: return asn1_parse_any(buf);
+      case 0x01: return asn1_parse_ascii_string(buf);
+      case 0x02: return asn1_parse_ascii_string(buf);
+      case 0x06: return asn1_parse_ascii_string(buf);
+      case 0x07: 
+        if (buf.length == 4) { // IPv4 address
+            return format_ipv4(buf);   
+        } else if (buf.length == 16) { // IPv6 address
+            return format_ipv6(buf);
+        }
+        return asn1_parse_any(buf);
+      default: return asn1_parse_any(buf);
+      }
+  }
+
+  return asn1_parse_any(buf);
+}
+
+function asn1_read(buf) {
+  var a = [];
+  var tag_class;
+  var tag;
+  var pointer = 0;
+  var is_constructed;
+  var s = "";
+  var length;
+
+  while (pointer < buf.length) {
+      // read type: 7 & 8 bits define class, 6 bit if it is constructed
+      s = buf[pointer];
+      tag_class = s >> 6;
+      is_constructed = s & 0x20;
+      tag = s & 0x1f;
+
+      if (tag == 0x1f) {
+          tag = 0;
+          var i = 0;
+
+          do {
+              if (i > 3)
+                  throw "Too big tag value" + tag;
+
+              i++;
+
+              if (++pointer >= buf.length)
+                  throw "Went out of buffer: " + pointer + " " + buf.length;
+
+              tag <<= 7;
+              tag += (buf[pointer] & 0x7f);
+
+          } while (buf[pointer] > 0x80)
+      }
+
+      if (++pointer > buf.length)
+           throw "Went out of buffer: " + pointer + " " + buf.length;
+
+      var lp = asn1_read_length(buf, pointer);
+      length = lp[0];
+      pointer = lp[1];
+
+      if ((pointer + length) > buf.length)
+           throw "length exceeds buf side: " + length + " " + pointer + " "
+               +  buf.length;
+
+      if (is_constructed) {
+          a.push(asn1_read(buf.slice(pointer, pointer + length)));
+
+      } else {
+          a.push(asn1_parse_primitive(tag_class, tag,buf.slice(pointer, pointer + length)));
+      }
+
+      pointer += length;
+  }
+
+  return a;
+}
+
+function is_oid_exist(cert, oid) {
+  for (var n = 0; n < cert.length; n++) {
+      if (Array.isArray(cert[n])) {
+          if (is_oid_exist(cert[n], oid))
+              return true;
+
+      } else {
+          if (cert[n] == oid)
+              return true;
+      }
+  }
+
+  return false;
+}
+
+// returns all the matching field with the specified 'oid' as a list
+function get_oid_value_all(cert, oid) {
+  var values = [];
+
+  for (var n = 0; n < cert.length; n++) {
+      if (Array.isArray(cert[n])) {
+          var r = get_oid_value_all(cert[n], oid);
+          if (r.length > 0) {
+          values = values.concat(r);
+          }
+      } else {
+          if (cert[n] == oid) {
+              if (n < cert.length) {
+                  // push next element in array
+                  values.push(cert[n+1]);
+              }
+          }
+      }
+  }
+
+  return values;
+}
+
+function get_oid_value(cert, oid) {
+  for (var n = 0; n < cert.length; n++) {
+      if (Array.isArray(cert[n])) {
+          var r = get_oid_value(cert[n], oid);
+          if (r !== false)
+              return r;
+
+      } else {
+          if (cert[n] == oid) {
+              if (n < cert.length) {
+                  // return next element in array
+                  return cert[n+1];
+              }
+          }
+      }
+  }
+
+  return false;
+}
+
+function parse_pem_cert(pem) {
+  var der = pem.split(/\n/);
+
+  if (pem.match('CERTIFICATE')) {
+      der = der.slice(1, -2);
+  }
+
+  return asn1_read(Buffer.from(der.join(''), 'base64'));
+}
+
+export default {asn1_read, parse_pem_cert, is_oid_exist, get_oid_value, get_oid_value_all};
